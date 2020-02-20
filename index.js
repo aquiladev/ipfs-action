4082c83d7f1c537a33d99e76e401a64d16b6c037
Sergii Bomko <aquila.ua@gmail.com>
2020-02-20 18:08:34 +0200

    formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const IpfsHttpClient = require('ipfs-http-client');
-const { globSource } = IpfsHttpClient
+const { globSource } = IpfsHttpClient;
 
 try {
   const path = core.getInput('path');
@@ -10,14 +10,14 @@ try {
   const protocol = core.getInput('protocol');
 
   const ipfs = IpfsHttpClient({ host, port, protocol });
-  
+
   ipfs.add(globSource(path, { recursive: true }))
     .then(result => {
-      const remoteDir = result[result.length - 1]
+      const remoteDir = result[result.length - 1];
       core.setOutput("hash", remoteDir.hash);
 
       // Get the JSON webhook payload for the event that triggered the workflow
-      const payload = JSON.stringify(github.context.payload, undefined, 2)
+      const payload = JSON.stringify(github.context.payload, undefined, 2);
       console.log(`The event payload: ${payload}`);
     }).catch(error => {
       core.setFailed(error.message);
