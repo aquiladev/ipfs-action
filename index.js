a4f635b57962b15d5e67fd5b94eb62aec4680a35
Sergii Bomko <aquila.ua@gmail.com>
2020-02-29 14:58:09 +0200

    removes try/catch

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 const uploader = require('./uploader');
 
 async function run() {
-  try {
+  // try {
     const path = core.getInput('path');
     const host = core.getInput('host');
     const port = core.getInput('port');
@@ -23,10 +23,10 @@ async function run() {
     }
 
     console.log('Upload to IPFS finished successfully', hash);
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
-  }
+  // } catch (error) {
+  //   core.error(error);
+  //   core.setFailed(error.message);
+  // }
 }
 
 run()
