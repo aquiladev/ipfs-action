0748d4c9523979f3c5c44ba07bb418dd7d287f4b
Sergii Bomko <aquila.ua@gmail.com>
2020-03-08 01:05:57 +0200

    rolls back one-by-one upload

--- uploader.js
+++ uploader.js
@@ -5,38 +5,23 @@ const { globSource } = IpfsHttpClient;
 
 async function _upload(options) {
   const { host, port, protocol, path, timeout, verbose } = options;
+  let rootHash;
 
   const root = fsPath.basename(path);
   const ipfs = IpfsHttpClient({ host, port, protocol, timeout });
-  const files = await all(globSource(path, { recursive: true })).catch((err) => { throw err; });
 
-  let rootHash;
-  for await (const file of files) {
-    const uploaded = await _uploadFile(file, ipfs, timeout);
-
+  for await (const file of ipfs.add(globSource(path, { recursive: true }), { pin: true, timeout })) {
     if (verbose)
-      console.log(uploaded.path, uploaded.cid.toString())
+      console.log(file.path, file.cid.toString())
 
-    if (root === uploaded.path)
-      rootHash = uploaded.cid.toString();
+    if (root === file.path)
+      rootHash = file.cid.toString();
   }
 
   if (!rootHash)
     throw new Error('Content hash is not found.');
 
   return rootHash;
-}
-
-async function _uploadFile(file, client, timeout, retry = 0) {
-  let source;
-  try {
-    source = await all(client.add(file, { pin: true, timeout }));
-  } catch (error) {
-    if (retry < 3)
-      return await _upload(file, client, timeout, retry + 1);
-    throw error;
-  }
-  return source && source[0];
 }
 
 function upload(options) {
