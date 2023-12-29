d8ffe021f609e7478e0f1c5914d4d1dff515cefb
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2023-12-29 10:54:20 -0600

    Add support for IPNS names

--- pinners/filebase.js
+++ pinners/filebase.js
@@ -1,59 +1,91 @@
-const fsPath = require("path");
-const { FilebaseClient } = require('@filebase/client')
-const { filesFromPath } = require('files-from-path')
+const fsPath = require("node:path");
+const { ObjectManager, NameManager } = require("@filebase/sdk");
+const { filesFromPath } = require("files-from-path");
 
 module.exports = {
-    name: "Filebase",
-    builder: async (options) => {
-        const { filebaseKey, filebaseSecret, filebaseBucket } = options;
+  name: "Filebase",
+  builder: async (options) => {
+    const { filebaseKey, filebaseSecret, filebaseBucket } = options;
 
-        if (!filebaseKey) {
-            throw new Error("filebaseKey is empty");
-        }
-
-        if (!filebaseSecret) {
-            throw new Error("filebaseSecret is empty");
-        }
-
-        if (!filebaseBucket) {
-            throw new Error("filebaseBucket is empty");
-        }
-
-        return { key: filebaseKey, secret: filebaseSecret, bucket: filebaseBucket };
-    },
-    upload: async (api, options) => {
-        console.log(`Parsing options...`);
-        const { path, pinName, verbose } = options;
-
-        let source = path;
-        if (!fsPath.isAbsolute(source)) {
-            const dir = (process.env.GITHUB_WORKSPACE || process.cwd()).toString();
-            source = fsPath.join(dir, source);
-        }
-
-        console.log(`Adding files...`);
-        const files = [];
-        for await (const file of filesFromPath(source, { pathPrefix: source })) {
-            files.push(file)
-            if (verbose) {
-                console.log(`Added File: ${JSON.stringify(file)}`);
-            }
-        }
-
-        console.log(`Storing files...`);
-        let tokenString = `${api.key}:${api.secret}:${api.bucket}`;
-        let cid = await FilebaseClient.storeDirectory(
-            { endpoint: 'https://s3.filebase.com', token: Buffer.from(tokenString).toString('base64') },
-            files,
-            pinName
-        );
-        console.log(`Stored files...`)
-        console.log(`CID: ${cid}`);
-        console.log(`Done`)
-
-        return {
-            cid: cid,
-            ipfs: cid,
-        };
+    if (!filebaseKey) {
+      throw new Error("filebaseKey is empty");
     }
+
+    if (!filebaseSecret) {
+      throw new Error("filebaseSecret is empty");
+    }
+
+    if (!filebaseBucket) {
+      throw new Error("filebaseBucket is empty");
+    }
+
+    return {
+      key: filebaseKey,
+      secret: filebaseSecret,
+      bucket: filebaseBucket,
+    };
+  },
+  upload: async (api, options) => {
+    console.log(`Parsing options...`);
+    const { path, pinName, verbose, key } = options;
+
+    let source = path;
+    if (!fsPath.isAbsolute(source)) {
+      const dir = (process.env.GITHUB_WORKSPACE || process.cwd()).toString();
+      source = fsPath.join(dir, source);
+    }
+
+    console.log(`Adding files...`);
+    const files = [];
+    for await (const file of filesFromPath(source, { pathPrefix: source })) {
+      files.push({ path: file.name, content: file.stream() });
+      if (verbose) {
+        console.log(`Added File: ${JSON.stringify(file)}`);
+      }
+    }
+
+    console.log(`Starting filebase client`);
+    const objectManager = new ObjectManager(api.key, api.secret, {
+      bucket: api.bucket,
+    });
+    console.log(`Started filebase client`);
+
+    console.log(`Storing files...`);
+    const uploadResult = await objectManager.upload(pinName, files),
+      cid = uploadResult.cid;
+    console.log(`Stored files...`);
+    console.log(`CID: ${cid}`);
+
+    // Set IPNS name
+    let ipnsKey = null;
+    if (key) {
+      const nameManager = new NameManager(api.key, api.secret),
+        existingName = await nameManager.get(key);
+      ipnsKey = existingName.network_key;
+
+      if (existingName === false) {
+        console.log(`Creating name...`);
+        await nameManager.create(key, cid);
+        console.log(`Created name...`);
+      } else {
+        console.log(`Updating name...`);
+        await nameManager.update(key, cid);
+        console.log(`Updated name...`);
+      }
+    }
+
+    console.log(`Done`);
+
+    if (ipnsKey) {
+      return {
+        cid: cid,
+        ipfs: cid,
+        ipns: ipnsKey,
+      };
+    }
+    return {
+      cid: cid,
+      ipfs: cid,
+    };
+  },
 };
