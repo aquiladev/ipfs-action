b605188cc86b4aacbbed374df947935e75337c38
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2024-01-04 10:32:07 -0600

    Add in Infura Pinner

--- pinners/index.js
+++ pinners/index.js
@@ -2,12 +2,12 @@ import make from "./maker.js";
 
 import ipfs from "./ipfs.js";
 import pinata from "./pinata.js";
-// import infura from "./infura.js";
+import infura from "./infura.js";
 import filebase from "./filebase.js";
 
 export default {
   ipfs: make(ipfs),
   pinata: make(pinata),
-  // infura: make(infura),
+  infura: make(infura),
   filebase: make(filebase)
 };
